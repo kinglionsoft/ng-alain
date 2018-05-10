e9665f8f15c82967049a5a17dbe54c24fa99b6d6
yangchao <yangchao@yun-kai.com>
2018-05-10 18:43:22 +0800

    add ou user

--- src/app/routes/organization/components/organization.component.ts
+++ src/app/routes/organization/components/organization.component.ts
@@ -1,17 +1,40 @@
 import { Component, OnInit, Injector } from '@angular/core';
 import { AppComponentBase } from '@shared';
-import { NzFormatEmitEvent, NzTreeNode } from 'ng-zorro-antd';
-import { OrganizationUnitClient, ListResultDtoOfOrganizationUnitDto, OrganizationUnitDto, CreateOrganizationUnitInput } from '@abp';
+import { NzFormatEmitEvent, NzTreeNode, NzModalService } from 'ng-zorro-antd';
+import { OrganizationUnitClient, ListResultDtoOfOrganizationUnitDto, OrganizationUnitDto, CreateOrganizationUnitInput, OrganizationUnitUserListDto } from '@abp';
 import { runInThisContext } from 'vm';
 
 @Component({
     selector: 'page-organization',
-    templateUrl: 'organization.component.html'
+    templateUrl: 'organization.component.html',
+    styles: [
+        `
+        .custom-filter-dropdown {
+          padding: 8px;
+          border-radius: 6px;
+          background: #fff;
+          box-shadow: 0 1px 6px rgba(0, 0, 0, .2);
+        }
+        .custom-filter-dropdown
+        [nz-input] {
+          width: 130px;
+          margin-right: 8px;
+        }
+        .highlight {
+          color: #f50;
+        }
+      `
+    ]
 })
 
 export class OrganizationComponent extends AppComponentBase implements OnInit {
 
+    private _selectedNode: NzTreeNode;
+
+    userNameSearchValue = '';
     nodes = [];
+    users: OrganizationUnitUserListDto[] = [];
+    displayUsers = [...this.users];
 
     constructor(injector: Injector,
         private client: OrganizationUnitClient
@@ -30,6 +53,33 @@ export class OrganizationComponent extends AppComponentBase implements OnInit {
             }, err => this.msgBox.error(err || '初始化组织机构失败'));
     }
 
+    //region  OU Tree
+    get selectedTitle(): string {
+        return this.selectedNode && this.selectedNode.title || '未选择组织';
+    }
+
+    get selectedNode(): NzTreeNode {
+        return this._selectedNode;
+    }
+
+    set selectedNode(node: NzTreeNode) {
+        if (!node) {
+            this._selectedNode = null;
+            this.users = [];
+            this.displayUsers = [];
+            return;
+        }
+        if (this._selectedNode && this._selectedNode.key === node.key) {
+            return;
+        }
+        this._selectedNode = node;
+        this.users = [];
+        this.client.getOrganizationUnitUsers(this._selectedNode.origin.id, '', 100, 0)
+            .subscribe(res => {
+                this.users = res.result.items;
+            });
+    }
+
     mouseAction(name: string, event: NzFormatEmitEvent): void {
         console.log(name, event);
         if (name === 'expand') {
@@ -38,24 +88,24 @@ export class OrganizationComponent extends AppComponentBase implements OnInit {
     }
 
     onNodeClick(event: NzFormatEmitEvent) {
-        console.dir(event);
+        this.selectedNode = event.node;
     }
 
-    addRoot() {
-        this.prompt('请输入组织名称')
+    addOrganization(parent: NzTreeNode) {
+        this.prompt('新增组织', '', '请输入组织名称')
             .then((value) => {
-                if (!this.validate(value)) {
-                    this.msgBox.error('仅支持中文、英文、数字');
-                    return;
-                }
                 this.client.createOrganizationUnit(
-                    <CreateOrganizationUnitInput>
-                    {
-                        parentId: null,
+                    <CreateOrganizationUnitInput>{
+                        parentId: parent && parent.origin.id,
                         displayName: value
                     })
                     .subscribe(response => {
-                        this.nodes.push(this.makeNode(response.result));
+                        const child = this.makeNode(response.result);
+                        if (!parent) { // 根组织
+                            this.nodes.push(this.makeNode(response.result));
+                        } else {
+                            parent.addChildren([child]);
+                        }
                     });
             });
     }
@@ -91,4 +141,45 @@ export class OrganizationComponent extends AppComponentBase implements OnInit {
     private getParentCode(code: string) {
         return code.substr(0, code.lastIndexOf('.'));
     }
+
+    private removeNode(code: string) {
+
+    }
+    //endregion
+
+    //region Organization
+
+    search(): void {
+        const filterFunc = (item) => {
+            return item.name.indexOf(this.userNameSearchValue) !== -1;
+        };
+        this.displayUsers = this.users.filter(item => filterFunc(item));
+    }
+
+    addUser(): void {
+        this.confirm('移除用户', `是否将中移除？`)
+            .subscribe(() => {
+                alert("ok");
+            });
+    }
+
+    deleteOrg(): void {
+        if (!this.selectedNode) return;
+        this.client.deleteOrganizationUnit(this.selectedNode.origin.id)
+            .subscribe(res => {
+                if (res.success) {
+                    this.removeNode(this.selectedNode.origin.code);
+                    this.selectedNode = null;
+                }
+            });
+    }
+
+    removeUser(user: OrganizationUnitUserListDto, index: number): void {
+        this.confirm('移除用户', `是否将【${user.name}】从【${this.selectedNode.title}】中移除？`)
+            .subscribe(() => {
+                this.displayUsers.splice(index, 1)
+            });
+    }
+
+    //endregion
 }
--- src/app/shared/app-component-base.ts
+++ src/app/shared/app-component-base.ts
@@ -1,11 +1,15 @@
+import { concat } from 'rxjs/observable/concat';
+import { inject } from '@angular/core/testing';
+import { Observable } from 'rxjs/Observable';
 import { Injector } from '@angular/core';
 import { AbpSettingService, LogService } from '@abp';
 import { NzMessageService, NzModalService } from 'ng-zorro-antd';
 import { NzModalPromptComponent } from './prompt/nz-modal-prompt.component';
+import { ModalHelper } from '@delon/theme';
 
 export abstract class AppComponentBase {
 
-    constructor(private injector: Injector) { }
+    constructor(protected injector: Injector) { }
 
     get msgBox(): NzMessageService {
         return this.injector.get(NzMessageService);
@@ -19,25 +23,52 @@ export abstract class AppComponentBase {
         return this.injector.get(LogService);
     }
 
-    get modal(): NzModalService {
-        return this.injector.get(NzModalService);
+    get modalHelper(): ModalHelper {
+        return this.injector.get(ModalHelper);
     }
 
-    prompt(title?: string, placeholder?: string): Promise<string> {
+    prompt(title?: string, value?: string, placeholder?: string): Promise<string> {
         return new Promise((resolve, reject) => {
-            const modal = this.modal.create({
+            const modal = this.injector.get(NzModalService).create({
                 nzTitle: title || '请输入',
                 nzContent: NzModalPromptComponent,
                 nzComponentParams: {
                     placeholder: placeholder || '请输入',
                 },
-                nzOnOk: (componentInstance) => resolve(componentInstance.value),
+                nzOnOk: (componentInstance) => {
+                    if (this.validate(componentInstance.value)) {
+                        resolve(componentInstance.value);
+                    } else {
+                        reject();
+                        this.msgBox.error('仅支持中文、英文、数字、-、_');
+                    }
+                },
                 nzOnCancel: () => reject()
             });
         });
     }
 
+    confirm(title: string, content: string) {
+        // return this.modalHelper.open(null, null, 'sm', {  nzTitle: title,
+        //     nzContent: content,
+        //  });
+        return this.modalHelper.static(null, null, 'sm', {
+            nzTitle: title,
+            nzContent: content,
+            nzFooter: [{
+                label: '确定', 
+                type: 'primary',
+                onClick: ()=>{
+                    this.injector.get(NzModalRef).close();
+                }
+            }, {
+                label: '取消', type: 'default'
+            }
+            ]
+        });
+    }
+
     validate(value: string): boolean {
-        return /^[\u4E00-\u9FA5A-Za-z0-9_]+$/.test(value);
+        return /^[\u4E00-\u9FA5A-Za-z0-9_-]+$/.test(value);
     }
 }
